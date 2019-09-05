30db6c9b81f189a3a6f16978ccb1c1958a5efd56
Paty Arakawa <paty.arakawa@gmail.com>
2019-09-05 14:54:50 -0500

    hook data sources

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,22 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
-const server = new ApolloServer({ typeDefs });
+const { createStore } = require('./utils');
+const LaunchAPI = require('./datasources/launch');
+const UserAPI = require('./datasources/user');
 
-server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+
+const store = createStore();
+
+    const server = new ApolloServer({
+    typeDefs,
+
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+        userAPI: new UserAPI({ store })
+        })
+    });
+
+    server.listen().then(({ url }) => {
+        console.log(`🚀 Server ready at ${url}`);
+    });
