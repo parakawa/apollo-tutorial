59e8d79973e2cfbee3e6f53fddb82a17431c4018
Paty Arakawa <paty.arakawa@gmail.com>
2019-09-06 14:09:19 -0500

    update data with mutations

--- start/client/src/index.js
+++ start/client/src/index.js
@@ -1,7 +1,6 @@
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import gql from "graphql-tag";
 
 import { ApolloProvider } from '@apollo/react-hooks';
 import React from 'react';
@@ -10,13 +9,23 @@ import Pages from './pages';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-	uri: 'http://localhost:4000/',	
+  uri: 'http://localhost:4000/',
+  headers: {
+    authorization: localStorage.getItem('token'),
+  },  	
 })
 
 const client = new ApolloClient({
 	cache,
 	link
 })
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
